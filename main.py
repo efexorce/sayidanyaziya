1e19a04eb32d14f46f83966d9f28a6e4e116085c
efexorce <mustafa.ulker@gmail.com>
2022-05-17 20:37:48 +0300

    tamamlandı

--- main.py
+++ main.py
@@ -13,24 +13,56 @@ rakamlar=(["", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", 
 onluk=(["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"])
 binlik=(["", "bin", "milyon", "milyar", "trilyon", "katrilyon","çok"])
 ###
-sayi=input()
-uzunluk=len(sayi)
-binindex=int((uzunluk-1)//3)
-basamak=uzunluk % 3
-if basamak==0: basamak=3
 yazi=[]
 
-for rakam in kelimeayraci(sayi):    
-    yazi.append(printci(basamak,int(rakam)))
-    if basamak == 3:
-        yazi.append("yüz")
-    basamak=basamak-1
-    if binindex>0:
-        if basamak == 0:
-            yazi.append(str(binlik[binindex]))
-            binindex=binindex-1
-            basamak=basamak+3
-tekkelime=""
-for eleman in yazi:
-    tekkelime=tekkelime+eleman
-print(tekkelime)
+def sayidanyaziya(sayi):
+    
+    uzunluk=len(sayi)
+    binindex=int((uzunluk-1)//3)
+    basamak=uzunluk % 3
+    if basamak==0: basamak=3
+
+    for rakam in kelimeayraci(sayi):   
+        if int(rakam)==1:
+            if int(basamak) ==3:
+                yazi.append("yüz")
+            if int(basamak) ==2:
+                yazi.append("on")         
+            if int(basamak) ==1:
+                yazi.append("bir")
+        
+        else:
+           
+            yazi.append(printci(basamak,int(rakam)))
+            if int(basamak) ==3:
+                if not int(rakam)==0:
+                    yazi.append("yüz")
+
+
+        basamak=basamak-1
+        if binindex>0:
+            if basamak == 0:
+                yazi.append(str(binlik[binindex]))
+                binindex=binindex-1
+                basamak=basamak+3
+
+    tekkelime=""
+    for eleman in yazi:
+        tekkelime=tekkelime+eleman
+    print(tekkelime)
+
+
+sayi=input()
+sayidanyaziya(sayi)
+yazi.clear()
+sayi=input()
+sayidanyaziya(sayi)
+yazi.clear()
+sayi=input()
+sayidanyaziya(sayi)
+yazi.clear()
+sayi=input()
+sayidanyaziya(sayi)
+yazi.clear()
+
+
