69b18c295aa80130cb50966f0ef2c5cbed3d4f42
Mustafa ÜLKER <45398740+efexorce@users.noreply.github.com>
2022-05-28 17:09:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def printci(basamak,rakam):
     if basamak==3:
         return str(rakamlar[rakam])
 ###
-rakamlar=(["", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"])
-onluk=(["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"])
-binlik=(["", "bin", "milyon", "milyar", "trilyon", "katrilyon","çok"])
+rakamlar=(["", "BİR", "İKİ", "ÜÇ", "DÖRT", "BEŞ", "ALTI", "YEDİ", "SEKİZ", "DOKUZ"])
+onluk=(["", "ON", "YİRMİ", "OTUZ", "KIRK", "ELLİ", "ALTMIŞ", "YETMİŞ", "SEKSEN", "DOKSAN"])
+binlik=(["", "BİN", "MİLYON", "MİLYAR", "TRİLYON", "KATRİLYON","çok"])
 ###
 yazi=[]
 
@@ -21,48 +21,49 @@ def sayidanyaziya(sayi):
     binindex=int((uzunluk-1)//3)
     basamak=uzunluk % 3
     if basamak==0: basamak=3
-
+    sayac=0
     for rakam in kelimeayraci(sayi):   
         if int(rakam)==1:
             if int(basamak) ==3:
-                yazi.append("yüz")
+                yazi.append("YÜZ")
             if int(basamak) ==2:
-                yazi.append("on")         
+                yazi.append("ON")         
             if int(basamak) ==1:
-                yazi.append("bir")
+                if int(binindex)!=1:
+                    yazi.append("BİR")
         
         else:
            
             yazi.append(printci(basamak,int(rakam)))
             if int(basamak) ==3:
                 if not int(rakam)==0:
-                    yazi.append("yüz")
+                    yazi.append("YÜZ")
 
 
         basamak=basamak-1
-        if binindex>0:
+        if int(rakam)==0:
+            sayac=sayac+1
+        else:
+            sayac=0
+
+
+        if (binindex>0):
             if basamak == 0:
-                yazi.append(str(binlik[binindex]))
+                if(sayac<3):
+                    yazi.append(str(binlik[binindex]))
                 binindex=binindex-1
                 basamak=basamak+3
 
     tekkelime=""
     for eleman in yazi:
         tekkelime=tekkelime+eleman
-    print(tekkelime)
+    yazi.clear()
+    return(tekkelime)
 
 
-sayi=input()
-sayidanyaziya(sayi)
-yazi.clear()
-sayi=input()
-sayidanyaziya(sayi)
-yazi.clear()
-sayi=input()
-sayidanyaziya(sayi)
-yazi.clear()
-sayi=input()
-sayidanyaziya(sayi)
-yazi.clear()
+sayi="1451"
+
+print(sayidanyaziya(sayi))
+#yazi.clear()
 
 
